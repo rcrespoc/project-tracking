d96d32cfc387b3ff93fca9422e220f3ebfdc789d
Richard Crespo <rjcrespo96@gmail.com>
2021-01-20 19:39:28 -0500

    Minificados

--- js/script.js
+++ js/script.js
@@ -1,16 +1,1 @@
-((d) => {
-  const $menuBtn = d.querySelector('.menu-btn'),
-  $menu = d.querySelector('.menu');
-  $menuBtn.addEventListener('click', e => {
-    $menuBtn.firstElementChild.classList.toggle('none');
-    $menuBtn.lastElementChild.classList.toggle('none');
-    $menu.classList.toggle('is-active');
-  });
-
-  d.addEventListener('click', e => {
-    if(!e.target.matches('.menu a')) return false;
-    $menuBtn.firstElementChild.classList.remove("none");
-    $menuBtn.lastElementChild.classList.add("none");
-    $menu.classList.remove("is-active");
-  })
-})(document);
+(e=>{const t=e.querySelector(".menu-btn"),s=e.querySelector(".menu");t.addEventListener("click",e=>{t.firstElementChild.classList.toggle("none"),t.lastElementChild.classList.toggle("none"),s.classList.toggle("is-active")}),e.addEventListener("click",e=>{if(!e.target.matches(".menu a"))return!1;t.firstElementChild.classList.remove("none"),t.lastElementChild.classList.add("none"),s.classList.remove("is-active")})})(document);
